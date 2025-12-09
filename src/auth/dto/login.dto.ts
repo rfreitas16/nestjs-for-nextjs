c779f6d7518ba6f85f1657bc7b522ec62c90ced6
rfreitas16 <rfreitas16@hotmail.com>
2025-12-09 15:27:03 -0300

    crianda validacoes com lib class-validator

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,4 +1,10 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
 export class LoginDto {
+  @IsEmail({}, { message: 'Email invalido' })
   email: string;
+
+  @IsString({ message: 'Senha precisa ser uma string' })
+  @IsNotEmpty({ message: 'senha nao pode estar vazia' })
   password: string;
 }
