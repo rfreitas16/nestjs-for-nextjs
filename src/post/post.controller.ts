ca399326f4821893f2d4d822892c1f07605ec46b
rfreitas16 <rfreitas16@hotmail.com>
2025-12-16 15:35:07 -0300

    criando rotas de ver posts

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { PostService } from './post.service';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import type { AuthenticatedRequest } from 'src/auth/types/authenticated-request';
@@ -14,5 +23,20 @@ export class PostController {
   async create(@Req() req: AuthenticatedRequest, @Body() dto: CreatePostDto) {
     const post = await this.postService.create(dto, req.user);
     return new PostResponseDto(post);
+  }
+  @UseGuards(JwtAuthGuard)
+  @Get('me/:id')
+  async findOneOwned(
+    @Req() req: AuthenticatedRequest,
+    @Param('id', ParseUUIDPipe) id: string,
+  ) {
+    const post = await this.postService.findOneOwnedOrFail({ id }, req.user);
+    return new PostResponseDto(post);
+  }
+  @UseGuards(JwtAuthGuard)
+  @Get('me')
+  async findAllOwned(@Req() req: AuthenticatedRequest) {
+    const posts = await this.postService.findAllOwened(req.user);
+    return posts.map(post => new PostResponseDto(post));
   }
 }
